cc401988f16a96d531fdd585b1f70333d7b8a499
Vishakha Mourya <vishakhamourya508@gmail.com>
2025-11-26 00:24:08 +0530

    deploy api added

--- client/src/config/env.js
+++ client/src/config/env.js
@@ -1,8 +1,8 @@
-export const env = {
-  apiBaseUrl: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
-  socketUrl: import.meta.env.VITE_SOCKET_URL || 'http://localhost:5000'
-};
 // export const env = {
-//   apiBaseUrl: import.meta.env.VITE_API_URL || 'https://real-time-editor-0a2s.onrender.com/api',
-//   socketUrl: import.meta.env.VITE_SOCKET_URL || 'https://real-time-editor-0a2s.onrender.com'
+//   apiBaseUrl: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+//   socketUrl: import.meta.env.VITE_SOCKET_URL || 'http://localhost:5000'
 // };
+export const env = {
+  apiBaseUrl: import.meta.env.VITE_API_URL || 'https://real-time-editor-0a2s.onrender.com/api',
+  socketUrl: import.meta.env.VITE_SOCKET_URL || 'https://real-time-editor-0a2s.onrender.com'
+};
