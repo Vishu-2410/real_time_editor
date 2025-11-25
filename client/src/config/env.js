ef0547375dff86e1209eefa93ee82c9917fc6af9
Vishakha Mourya <vishakhamourya508@gmail.com>
2025-11-26 00:23:08 +0530

    project added

--- client/src/config/env.js
+++ client/src/config/env.js
@@ -3,6 +3,6 @@ export const env = {
   socketUrl: import.meta.env.VITE_SOCKET_URL || 'http://localhost:5000'
 };
 // export const env = {
-//   apiBaseUrl: import.meta.env.VITE_API_URL || 'https://real-time-collaborator.onrender.com/api',
-//   socketUrl: import.meta.env.VITE_SOCKET_URL || 'https://real-time-collaborator.onrender.com'
+//   apiBaseUrl: import.meta.env.VITE_API_URL || 'https://real-time-editor-0a2s.onrender.com/api',
+//   socketUrl: import.meta.env.VITE_SOCKET_URL || 'https://real-time-editor-0a2s.onrender.com'
 // };
